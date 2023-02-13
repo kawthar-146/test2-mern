24367f5ff4b0e2dcad16d564575102087509284f
kawthar-146 <kawtharzeayter.6@gmail.com>
2023-02-13 13:41:21 +0200

    Done

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { AppBar, Box, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [value, setValue] = useState();
+
+  return (
+    <div>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h3">Test2</Typography>
+          <Box sx={{ marginLeft: "auto" }}>
+            <Tabs
+              indicatorColor="secondary"
+              onChange={(e, val) => setValue(val)}
+              value={value}
+              textColor="inherit"
+            >
+                 <Tab  to="/login" LinkComponent={Link} label="Login" />
+                  <Tab to="/signup" LinkComponent={Link}  label="Signup" />
+            </Tabs>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
